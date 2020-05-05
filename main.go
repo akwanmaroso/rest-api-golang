2f33d91c5674c8e9f03637934db11c5854c1c272
akwanmaroso <akwanmaroso@gmail.com>
2020-05-05 10:06:21 +0800

    ref(main): add route list

--- main.go
+++ main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
-	"fmt"
-	_"net/http"
+	"database/sql"
+	_"github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"net/http"
 )	
 
+var db *sql.DB
+var err error
+
 func main() {
-	fmt.Println("Hello World")
+	db, err := sql.Open("mysql", "root:@/go_rest_api")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/posts", getPosts).Methods("GET")
+	router.HandleFunc("/posts", createPost).Methods("POST")
+	router.HandleFunc("/posts/{id}", getPost).Methods("GET")
+	router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
+	router.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
+
+	http.ListenAndServe(":8080", router)
 }
 

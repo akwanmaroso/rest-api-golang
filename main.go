40878358f0627a8d8c18ddf5100c610f0b030893
akwanmaroso <akwanmaroso@gmail.com>
2020-05-05 10:29:46 +0800

    feat(main): add func crud

--- main.go
+++ main.go
@@ -5,7 +5,15 @@ import (
 	_"github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"net/http"
-)	
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+type Post struct {
+	ID		string	`json:"id"`
+	Title	string	`json:"title"`
+}
 
 var db *sql.DB
 var err error
@@ -20,12 +28,75 @@ func main() {
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/posts", getPosts).Methods("GET")
+	router.HandleFunc("/", getPosts).Methods("GET")
 	router.HandleFunc("/posts", createPost).Methods("POST")
 	router.HandleFunc("/posts/{id}", getPost).Methods("GET")
-	router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
-	router.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
+	//router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
+	//router.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
 
 	http.ListenAndServe(":8080", router)
 }
 
+func getPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	result, err := db.Query("SELECT id, title FROM posts WHERE id = ?", params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+	var post Post
+	for result.Next() {
+		err := result.Scan(&post.ID, &post.Title)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	json.NewEncoder(w).Encode(post)
+}
+
+func createPost(w http.ResponseWriter, r *http.Request) {
+	stmt, err := db.Prepare("INSERT INTO posts(title) VALUES(?)")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	title := keyVal["title"]
+
+	_, err = stmt.Exec(title)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println(w, "New Post Created")
+}
+
+func getPosts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application-json")
+
+	var posts []Post
+
+	result, err := db.Query("SELECT * FROM posts")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer result.Close()
+
+	for result.Next() {
+		var post Post
+		err := result.Scan(&post.ID, &post.Title)
+		if err != nil {
+			panic(err.Error())
+		}
+		posts = append(posts, post)
+	}
+	json.NewEncoder(w).Encode(posts)
+}
